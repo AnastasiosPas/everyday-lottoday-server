0dd4ca85869bb8ab07b4b784399d4a710182dddd
AnastasiosPas <tassos_pasalis@mac.com>
2021-11-14 12:23:06 +0100

    adding postgreSQL through heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import knex from 'knex';
 const db =  knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1', //our home - localhost
+      host : 'postgresql-infinite-58070', //our home - localhost
       //port : 3002,
       user : 'anastasiospasalis',
       password : '',
